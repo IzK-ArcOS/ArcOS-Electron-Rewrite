b183def3072aab58e7588a559bf64f1dade8d19a
TechWorldInc <76709090+TechWorldInc@users.noreply.github.com>
2022-03-09 20:06:20 +0100

    Continue work on Desktop

--- packages/renderer/src/lib/desktop/Taskbar.svelte
+++ packages/renderer/src/lib/desktop/Taskbar.svelte
@@ -1,49 +1,72 @@
 <script lang="ts">
-  import { getUserData, isUser, TaskbarPosition, Theme } from "../ts/userLogic";
+  import "../../css/general.scss";
+  import systemIcon from "../../img/systemIcon.svg";
+  import { getUserData, setUserPreference, TaskbarPosition, Theme } from "../ts/userLogic";
   import type { UserTemplate } from "../ts/userLogic";
   import ActionCenter from "./ActionCenter.svelte";
   import Startmenu from "./Startmenu.svelte";
   import buf from "buffer/";
   import { Themes } from "../ts/themeLogic";
-  import { userDataStore } from "../ts/stores";
+  import {
+    actCenterOpened,
+    startMenuOpened,
+    userDataStore,
+  } from "../ts/stores";
+  import type { Writable } from "svelte/store";
 
   export let username: string;
 
   const Buffer = buf.Buffer;
-  const userExists = isUser(Buffer.from(username).toString("base64"));
 
   let theme: Theme = Theme.darkround;
-  let tbtop: boolean = false;
+  let tbbtm: boolean = false;
   let backg: string;
-  let border: string;
+  let docked: boolean = false;
+  let startMenuOpen: boolean;
+  let actCenterOpen: boolean;
 
   function update(input: UserTemplate | undefined) {
     const userData: UserTemplate = input as UserTemplate;
 
     theme = userData.theme;
-    tbtop = userData.taskbarPos == TaskbarPosition.top;
+    tbbtm = userData.taskbar.position
+      ? userData.taskbar.position == TaskbarPosition.bottom
+      : false;
     backg = Themes.get(theme)!.variables.taskbarBackground;
-    border = Themes.get(theme)!.variables.windowBorder;
+    docked = userData.taskbar.docked as boolean;
   }
-
-  userDataStore.subscribe(update);
 
   update(getUserData(username) as UserTemplate);
 
-  console.log(theme, getUserData(username));
+  userDataStore.subscribe(update);
+  startMenuOpened.subscribe((value) => (startMenuOpen = value));
+  actCenterOpened.subscribe((value) => (actCenterOpen = value));
 
-  let startMenuOpened: boolean;
-  let actnCentrOpened: boolean;
+  console.log(startMenuOpened);
+  const toggleStart = () => startMenuOpened.set(!startMenuOpen);
+
+  setTimeout(() => {
+    const taskbarData = (getUserData(username) as UserTemplate).taskbar;
+
+    taskbarData.docked = true;
+
+    setUserPreference(username,"taskbar",taskbarData)
+  }, 3000);
 </script>
 
 <div
   class="taskbar"
-  class:ontop={tbtop}
-  style="background-color: {backg}; border: {border};"
+  class:ontop={!tbbtm}
+  class:docked
+  style="background-color: {backg};"
 >
-  <Startmenu />
-  <ActionCenter />
+  <button class="startbutton" on:click={toggleStart}>
+    <img src={systemIcon} alt="startButton" />
+  </button>
 </div>
+
+<Startmenu visible={startMenuOpen} {username} />
+<ActionCenter />
 
 <style scoped>
   div.taskbar {
@@ -51,12 +74,29 @@
     bottom: 10px;
     left: 10px;
     width: calc(100% - 20px);
-    height: 35px;
+    height: 40px;
     border-radius: 7.5px;
   }
 
   div.taskbar.ontop {
     top: 10px;
     bottom: unset;
+  }
+
+  div.taskbar.docked {
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 35px;
+  }
+
+  button.startbutton {
+    background-color: transparent;
+    border: none;
+    padding: 7px;
+  }
+
+  button.startbutton img {
+    height: 25px;
   }
 </style>
